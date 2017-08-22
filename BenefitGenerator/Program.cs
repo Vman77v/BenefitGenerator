a4a033e8312b0243c724d17da29b38bb69afd717
vman77v <v_viayra@yahoo.com>
2017-08-21 18:12:27 -0600

    Adding extended level of coverage mapping

--- BenefitGenerator/Program.cs
+++ BenefitGenerator/Program.cs
@@ -31,6 +31,17 @@ namespace BenefitGenerator
                 ["VS"] = 2,
                 ["VC"] = 4,
                 ["VF"] = 5,
+                ["EMP"] = 1,["ESP"] = 2,["E1D"] = 3,["ECH"] = 4,["FAM"] = 5, //Main Levels of coverage
+                ["TWO"] = 0,                
+                ["E3D"] = 3,
+                ["SPO"] = 6,
+                ["E5D"] = 6,
+                ["CHD"] = 7,
+                ["E2D"] = 7,
+                ["E7D"] = 8,
+                ["SPC"] = 8,
+                ["E8D"] = 8,
+                ["E6D"] = 9
             };
 
             return benefitDictionary;
@@ -38,6 +49,8 @@ namespace BenefitGenerator
 
         private static void Main(string[] args)
         {
+            var inputFile = Environment.CurrentDirectory = "Benefit.txt";
+
 
             var benfitCategory = "MM";
             var primaryBenefits = "EE, ES, EC, EF";
